e12caab25603f328fc6fa4a84dc91e9439786238
madalin12333 <irimiamadalinvasile@gmail.com>
2022-02-14 15:28:13 +0200

    Update datacenter.py

--- data_structures/datacenter.py
+++ data_structures/datacenter.py
@@ -4,23 +4,23 @@ class Datacenter:
     
     def __init__(self, name, cluster_dict):
 
-        self.name = str(name)
-        self.cluster_dict = list(cluster_dict)
-        """
+         """
         Constructor for Datacenter data structure.
 
         self.name -> str
         self.clusters -> list(Cluster)
         """
-
-        pass
+            
+        self.name = str(name)
+        self.cluster_dict = list(cluster_dict)
 
     def remove_invalid_clusters(self):
 
-        pattern = r"\b" + re.escape(self.name[:3].upper()) + r"-[0-9]{1,3}\b"
-        self.cluster_dict = [ elem for elem in self.cluster_dict if re.match(pattern, elem) ]
         
         """
         Removes invalid objects from the clusters list.
         """
-        pass
+        
+        pattern = r"\b" + re.escape(self.name[:3].upper()) + r"-[0-9]{1,3}\b"
+        self.cluster_dict = [ elem for elem in self.cluster_dict if re.match(pattern, elem) ]
+       
